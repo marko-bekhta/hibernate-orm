88d989149dbb6b7a53807ef7d17124d577fbf542
Andrea Boriero <andrea@hibernate.org>
2026-05-05 09:51:45 +0200

    fix few NPE caused by null writer

--- hibernate-core/src/main/java/org/hibernate/property/access/spi/EnhancedSetterImpl.java
+++ hibernate-core/src/main/java/org/hibernate/property/access/spi/EnhancedSetterImpl.java
@@ -4,15 +4,14 @@
  */
 package org.hibernate.property.access.spi;
 
-import java.io.Serial;
-import java.io.Serializable;
-import java.lang.reflect.Field;
-
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.hibernate.Internal;
 import org.hibernate.accessor.HibernateAccessorFactory;
 import org.hibernate.property.access.internal.AbstractFieldSerialForm;
 
-import org.checkerframework.checker.nullness.qual.Nullable;
+import java.io.Serial;
+import java.io.Serializable;
+import java.lang.reflect.Field;
 
 import static org.hibernate.property.access.internal.AccessStrategyHelper.determineEnhancementState;
 import static org.hibernate.property.access.internal.AccessStrategyHelper.handleEnhancedInjection;
