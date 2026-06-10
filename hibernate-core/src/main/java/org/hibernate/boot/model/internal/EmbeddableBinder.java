615eb26e0c9d6222a88395439134db5cf4115668
Andrea Boriero <andrea@hibernate.org>
2026-06-10 20:52:41 +0200

    Fix DerivedIdentityEmbeddedIdParentEmbeddedIdGrandparentEmbeddedIdColumnOverridesDepTest failure

--- hibernate-core/src/main/java/org/hibernate/boot/model/internal/EmbeddableBinder.java
+++ hibernate-core/src/main/java/org/hibernate/boot/model/internal/EmbeddableBinder.java
@@ -1276,6 +1276,7 @@ public class EmbeddableBinder {
 				Property referencedProperty ) {
 			final var property = new Property();
 			property.setName( referencedProperty.getName() );
+			property.setMemberDetails( referencedProperty.getMemberDetails() );
 			//FIXME set optional?
 			//property.setOptional( property.isOptional() );
 			property.setPersistentClass( embeddable.getOwner() );
